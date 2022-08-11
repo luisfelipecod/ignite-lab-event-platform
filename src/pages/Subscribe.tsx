e4740cb9a9ce937c11b2af3d4440494e1d94df6b
Luis Felipe <luisfelipe.tbr@gmail.com>
2022-08-11 13:21:56 -0300

    update in background image path from page  subscriber

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import MokupCode from '../assets/code-mockup.png';
 
 
 export function Subscribe(){
@@ -65,7 +66,7 @@ export function Subscribe(){
                 </div>
 
             </div>
-            <img src="./src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={MokupCode} className="mt-10" alt="" />
         </div>
     )
 }
